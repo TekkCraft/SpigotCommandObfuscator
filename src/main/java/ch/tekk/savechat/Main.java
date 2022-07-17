e8f5b6739afe0c53657a61cfaa17c8826730b3aa
Tekk <marc.wilhelm@orca.ch>
2022-07-17 17:55:53 +0200

    Init

---
+++ src/main/java/ch/tekk/savechat/Main.java
@@ -0,0 +1,29 @@
+package ch.tekk.savechat;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+import ch.tekk.savechat.listeners.ChatListener;
+
+public class Main extends JavaPlugin {
+    private FileConfiguration config;
+
+    @Override
+    public void onEnable() {
+        this.ensureConfig();
+
+        this.getServer().getPluginManager().registerEvents(new ChatListener(), this);
+    }
+
+    @Override
+    public void onDisable() {
+    }
+
+    /**
+     * Ensures the existence of a config file.
+     */
+    private void ensureConfig() {
+        this.saveDefaultConfig();
+        this.reloadConfig();
+        this.config = this.getConfig();
+    }
+}
---
+++ src/main/java/ch/tekk/savechat/listeners/ChatListener.java
@@ -0,0 +1,20 @@
+package ch.tekk.savechat.listeners;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+public class ChatListener implements Listener {
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerChat(AsyncPlayerChatEvent event) {
+        Player player = event.getPlayer();
+        String message = event.getMessage();
+
+        Bukkit.broadcastMessage("[" + player.getDisplayName() + "] " + message);
+        event.setCancelled(true);
+    }
+}
